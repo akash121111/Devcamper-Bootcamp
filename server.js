b7de4793e6b784d5dd423974f8efca3595bec096
akash121111 <akashkumar121111@gmail.com>
2020-03-27 18:31:19 +0530

    create bootcamp api

--- server.js
+++ server.js
@@ -6,8 +6,24 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ name: 'akash' });
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'show all Bootcamp' });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'create new Bootcamp' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `update Bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete Bootcamp ${req.params.id}` });
 });
 
 const PORT = process.env.PORT || 5000;
