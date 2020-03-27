4f9cbb136026810fc8163c8b468f457366d4a65e
akash121111 <akashkumar121111@gmail.com>
2020-03-27 23:34:15 +0530

    database are connected now but promise rejection not done

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+//load env vars
+dotenv.config({ path: './config/config.env' });
+
+//connect database
+
+connectDB();
 
 //routes files
 
 const bootcamps = require('./routes/bootcamps');
-
-//load env vars
-dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
@@ -23,7 +28,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//handle unhandle promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error:${err.message}`);
+  //close server & exit process
+  server.close(() => process.exit(1));
+});
