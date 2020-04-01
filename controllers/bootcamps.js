9f0afd9729703bd5f738dfd79a39452a5c5739a9
akash121111 <akashkumar121111@gmail.com>
2020-04-01 06:33:11 +0530

    filtering lte gte and lt also and query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,7 +7,14 @@ const ErrorResponce = require('../utils/errorResponse');
 //@routes   GET /api/v1/bootcamps
 //@access   public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  let queryStr = JSON.stringify(req.query);
+
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  query = Bootcamp.find(JSON.parse(queryStr));
+  const bootcamps = await query;
   res.status(200).json({
     success: true,
     count: bootcamps.length,
